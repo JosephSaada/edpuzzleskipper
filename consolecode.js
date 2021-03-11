15fd39b2348c5a05dc7343e9f184fb92f6d0d4ec
Joseph Saada <64779012+JosephSaada@users.noreply.github.com>
2021-03-11 10:39:57 -0800

    Update consolecode.js

--- consolecode.js
+++ consolecode.js
@@ -1,4 +1,4 @@
-speed = 5
+speed = 3
 video = document.querySelector('video')
 Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'playbackRate').set.call(video, speed)
 Object.defineProperty(video, 'playbackRate', { writable: false })
